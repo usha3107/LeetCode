283ac772e798225883b14c4fe5cda2543107820e
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-04-05 10:32:46 +0530

    Time: 21 ms (63.99%), Space: 54 MB (80.65%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.java
@@ -0,0 +1,21 @@
+class RecentCounter {
+    int size=0;
+    Deque<Integer>deque;
+    public RecentCounter() {
+        deque=new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+       deque.add(t);
+       while(t-deque.peekFirst()>3000){
+        deque.pollFirst();
+       }
+       return deque.size();
+    } 
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
