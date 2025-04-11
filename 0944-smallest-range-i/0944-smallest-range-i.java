1122f0fe362099b09561b8d33c55f54690748b8a
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-04-11 10:52:00 +0530

    Time: 2 ms (77.51%), Space: 45.5 MB (71.03%) - LeetHub

---
+++ 0944-smallest-range-i/0944-smallest-range-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            } else if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return (max - k) - (min + k) < 0 ? 0 : (max - k) - (min + k);
+    }
+}
