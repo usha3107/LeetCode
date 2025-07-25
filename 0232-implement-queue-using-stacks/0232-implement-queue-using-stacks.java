69cbafb7d8a5d59648fae2464c10e9a7d638c37d
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-07-25 20:22:54 +0530

    Time: 0 ms (100%), Space: 41.4 MB (51.54%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class MyQueue {
+
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+    
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    public int pop() {
+        peek(); // Ensure output stack has the current front element
+        return output.pop();
+    }
+    
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+    
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
