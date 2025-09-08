8ae7bc16a6ffc642854b787149035ad2ca2c6537
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-09-08 16:00:00 +0530

    Time: 1652 ms (11.66%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT p.product_name, s.year, s.price  
-FROM Sales s  
-JOIN Product p  
-ON s.product_id = p.product_id;
+SELECT product_name, year, price
+FROM Sales
+JOIN Product USING(product_id);
