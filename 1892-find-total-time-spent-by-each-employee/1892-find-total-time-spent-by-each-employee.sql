1e2c8553a2efba78a24df24ca862e90e95ad4103
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-09-17 22:06:40 +0530

    Time: 514 ms (60.61%), Space: 0B (100%) - LeetHub

--- 1892-find-total-time-spent-by-each-employee/1892-find-total-time-spent-by-each-employee.sql
+++ 1892-find-total-time-spent-by-each-employee/1892-find-total-time-spent-by-each-employee.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-SELECT 
-    event_day AS day, 
-    emp_id, 
-    SUM(out_time - in_time) AS total_time
-FROM 
-    employees
-GROUP BY 
-    event_day, emp_id;
+SELECT event_day AS day,
+emp_id,
+SUM(out_time - in_time) AS total_time
+FROM Employees
+GROUP BY emp_id,
+event_day
+ORDER BY day,
+emp_id;
