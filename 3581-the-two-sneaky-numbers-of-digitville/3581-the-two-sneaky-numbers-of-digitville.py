c98d11856164097ebc7f9ef92789de35c1999f91
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-10-03 18:53:28 +0530

    Time: 4 ms (25.06%), Space: 17.7 MB (91.89%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,35 @@
+class Solution:
+    def getSneakyNumbers(self, nums):
+        xor_sum = 0
+        total_size = len(nums)
+        actual_size = len(nums) - 2
+
+        # XOR all elements of the array
+        for num in nums:
+            xor_sum ^= num
+
+        # XOR all numbers from 0 to actual_size - 1
+        for i in range(actual_size):
+            xor_sum ^= i
+
+        # Find the rightmost set bit in xor_sum
+        rightmost_set_bit = xor_sum & ~(xor_sum - 1)
+
+        first_sneaky_number = 0
+        second_sneaky_number = 0
+
+        # Separate the numbers into two groups based on the rightmost set bit
+        for num in nums:
+            if num & rightmost_set_bit:
+                first_sneaky_number ^= num
+            else:
+                second_sneaky_number ^= num
+
+        # XOR the range of numbers from 0 to actual_size - 1
+        for i in range(actual_size):
+            if i & rightmost_set_bit:
+                first_sneaky_number ^= i
+            else:
+                second_sneaky_number ^= i
+
+        return [first_sneaky_number, second_sneaky_number]
