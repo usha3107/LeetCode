5d5a4d5c0886406071b44785f941f127223e0d2e
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-10-05 19:00:49 +0530

    Time: 593 ms (23.48%), Space: 0B (100%) - LeetHub

--- 1837-daily-leads-and-partners/1837-daily-leads-and-partners.sql
+++ 1837-daily-leads-and-partners/1837-daily-leads-and-partners.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-select date_id,make_name,count(distinct lead_id) as unique_leads,
-count(distinct partner_id) as unique_partners
-from DailySales
-group by make_name,date_id
+SELECT date_id,
+       make_name,
+       count(DISTINCT lead_id) AS unique_leads,
+       count(DISTINCT partner_id) AS unique_partners
+  FROM DailySales
+ GROUP BY date_id,
+          make_name;
