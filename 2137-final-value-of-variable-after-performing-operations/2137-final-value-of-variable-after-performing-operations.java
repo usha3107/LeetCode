597b01b662bc3fe9063a6cbfebd34624e7924c78
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-10-09 17:35:48 +0530

    Time: 1 ms (74.32%), Space: 42.8 MB (46.52%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X =0;
+        for(String i : operations){
+            if(i.equals("--X") || i.equals("X--")){
+                X--;
+            }
+            else{
+                X++;
+            }
+        } 
+        return X;
+    }
+}
+
+
