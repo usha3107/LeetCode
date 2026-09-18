0223bfbba3f373cf441ac67bbca4037fb6416bcd
KILARI USHODAYA <23mh1a05h8@acoe.edu.in>
2026-09-18 12:14:26 +0530

    Time: 1 ms (76.86%), Space: 42 MB (87.71%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isThree(int n) {
+        int total = 0;
+        for(int i = 2; i < n; ++i)
+        {
+            if(n % i == 0) total++;
+        }
+        if(total == 1) return true;
+        return false;
+        
+    }
+}
